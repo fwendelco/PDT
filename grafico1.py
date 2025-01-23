281f78434d1cf8e020eabf9900b3e26101d370cd
fwendelco <65903605+fwendelco@users.noreply.github.com>
2025-01-23 14:12:01 -0300

    grafico1.py

--- grafico1.py
+++ grafico1.py
@@ -15,7 +15,7 @@ st.image("cr.jpeg")
 # Configuração do título da aplicação
 st.markdown(
     """
-    <div style='text-align: center; font-size: 35px; font-weight: bold;'>
+    <div style='text-align: center; font-size: 38px; font-weight: bold;'>
         Relatório Socioemocional por Escola
     </div>
     """,
